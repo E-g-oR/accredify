ba97d6a717f49f049a931fc8aed9fb4c17ce9c71
Egor Muzychkin <emuzych@gmail.com>
2024-08-18 19:30:22 +0300

    feat: add useFetch hook

---
+++ src/shared/lib/hooks/useFetch.ts
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useState } from "react";
+
+export const useFetch = <Data = unknown, Error = unknown>(url: string) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<Data | undefined>();
+  const [error, setError] = useState<Error | undefined>();
+
+  const run = useCallback(() => {
+    setIsLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then(setData)
+      .catch(setError)
+      .finally(() => setIsLoading(false));
+  }, [url]);
+
+  useEffect(() => {
+    run();
+  }, [run]);
+
+  return {
+    isLoading,
+    data,
+    error,
+    run,
+  };
+};
