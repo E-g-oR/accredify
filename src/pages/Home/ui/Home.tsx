902cd17127c0a46f604de19c226ed3c34e33fed2
Egor Muzychkin <emuzych@gmail.com>
2024-08-19 11:26:48 +0300

    feat: request user from api

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -2,18 +2,32 @@ import { FC } from "react";
 
 import { CareerGoal } from "./CareerGoal";
 import { RecentDocuments } from "./RecentDocuments";
+import { useFetch } from "shared/lib";
+import { Show } from "shared/ui/Show";
+import { Skeleton } from "shared/ui/Skeleton";
+import { IResponse } from "shared/model/types/Response";
+import { IUser } from "src/entities/User";
 
 export const Home: FC = () => {
+  const { data, isLoading } = useFetch<IResponse<IUser>>(
+    "https://raw.githubusercontent.com/Phangster/Accredify-FE-Interview-Endpoint/main/user-personal.json"
+  );
   return (
     <>
       <div className={"mb-14"}>
-        <h1 className={"text-3xl font-bold mb-2"}>Hi, Gerald Goh 👋🏻</h1>
+        <Show when={!isLoading} fallback={<Skeleton className={"w-80 h-8"} />}>
+          <h1 className={"text-3xl font-bold mb-2"}>
+            Hi, {data?.data?.name || "User"} 👋🏻
+          </h1>
+        </Show>
         <p className={"text-slate-500"}>
           Manage your documents issued by SMU Academy or Track your career goal.
         </p>
       </div>
       <div className={"gap-7 flex "}>
-        <CareerGoal />
+        <Show when={!!data && !data.data.current_organisation.is_personal}>
+          <CareerGoal />
+        </Show>
         <RecentDocuments />
       </div>
     </>
