6689df942db91c24539f25b57df797e4b552bf2f
Egor Muzychkin <emuzych@gmail.com>
2024-08-21 10:29:21 +0300

    test: add tests for Button component

--- src/shared/ui/Button/ui/Button.test.jsx
+++ src/shared/ui/Button/ui/Button.test.jsx
@@ -4,9 +4,24 @@
 import { render, screen } from "@testing-library/react";
 import { Button } from "./Button";
 import "@testing-library/jest-dom";
+import user from "@testing-library/user-event";
 
-test("Button rendered to the screen", () => {
-  render(<Button>Click me</Button>);
-  const btn = screen.getByText(/click me/i);
-  expect(btn).toBeInTheDocument();
+describe("Button component", () => {
+  test("Button rendered to the screen", () => {
+    render(<Button>Click me</Button>);
+    const btn = screen.getByText(/click me/i);
+    expect(btn).toBeInTheDocument();
+  });
+
+  test("Button onClick function has been called", async () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>click</Button>);
+    const button = screen.getByText(/click/i);
+    expect(button).toHaveRole("button");
+
+    await user.click(button);
+
+    expect(onClick).toHaveBeenCalled();
+    expect(button).toHaveFocus();
+  });
 });
